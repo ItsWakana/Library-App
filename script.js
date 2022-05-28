1cc9e3eb0727eb4e132eb4b0e05f6985ccb0a15f
MultiFlex <wakanawardrobe@gmail.com>
2022-05-28 18:19:03 +0100

    added basic css layout for books

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let myLibrary = [];
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 310);
+const fellowship = new Book('The Fellowship of the Ring', 'J.R.R Tolkien', 423);
+
+
+let myLibrary = [theHobbit, fellowship];
 
 function Book(title,author,pages) {
     this.title = title;
@@ -6,6 +10,16 @@ function Book(title,author,pages) {
     this.pages = pages;
 }
 
+const bookContainer = document.querySelector('.books');
+
 function addBookToLibrary() {
-    
+
+    // myLibrary.forEach((bookTitle) => {
+    //     let book = document.createElement('p');
+    //     book.textContent =
+    //     bookContainer.appendChild(book);
+    // });
+
 }
+
+addBookToLibrary();
